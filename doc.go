b712cbe9d8f6848e2adfb538af87466832a7007f
Adam S Levy <adam@aslevy.com>
2018-08-31 15:38:04 -0800

    chore(copyright): Add copyright to doc.go & doc_test.go, remove version number

--- doc.go
+++ doc.go
@@ -1,3 +1,8 @@
+// github.com/canonical-ledgers/bitcointax
+// Copyright 2018 Canonical Ledgers, LLC. All rights reserved.
+// Use of this source code is governed by the MIT license that can be found in
+// the LICENSE file distributed with this source code.
+
 // Package bitcointax implements a Transaction type which properly Marshals and
 // Unmarshals into the valid JSON format for the bitcoin.tax REST API, as well
 // as a Client with methods for listing and adding Transactions.
